5455da4fb3391cc45227a4c0be9df83183cf9604
stbam <55661516+stbam@users.noreply.github.com>
2024-07-13 19:53:40 -0400

    Initial save point

---
+++ src/components/AppBar.jsx
@@ -0,0 +1,22 @@
+import { View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor:'Black'
+    // ...
+  },
+  // ...
+});
+
+const AppBar = () => {
+  return (
+    <>
+    <View style={styles.container}>{/* ... */}</View>;
+  </>
+  )
+
+};
+
+export default AppBar;
---
+++ src/components/Main.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+import RepositoryList from './RepositoryList'; // Adjust the path if necessary
+import BigBlueText from './BlueText';
+import AppBar from './AppBar';
+const styles = StyleSheet.create({
+
+});
+
+const Main = ({ repositories }) => {
+  return (
+    <View>
+      <Text>Rate Repository Application</Text>
+      <RepositoryList repositories={repositories} />
+      <BigBlueText></BigBlueText>
+    </View>
+  )
+};
+
+export default Main;
